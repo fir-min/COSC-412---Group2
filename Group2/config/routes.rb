0de7bf0433c50062425ff369b6cdb978482254c6
b3bry <bateoa1@students.towson.edu>
2016-04-05 16:55:35 -0400

    Removed redundant code in routes.rb
    Added logical routing based on whether the user is authenticated or not.

--- Group2/config/routes.rb
+++ Group2/config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  get 'budget/overall'
-
-  get 'home/index'
-
- # get 'admin_home/admin_home'
-
   devise_for :users
   match ':controller(/:action(/:id(.:format)))', :via => :get
   
@@ -12,7 +6,25 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'home#index'
+  
+  
+  
+  devise_scope :user do
+    authenticated :user do
+      root 'budget#overall', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root 'home#index', as: :unauthenticated_root
+    end
+  end
+  
+  
+  
+  
+  
+  
+  
   
 
   # Example of regular route:
