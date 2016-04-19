35a8d87d51dcbc5b8325c882a165a1d95b31b2ba
Ted Smith III <tsmith1119s@gmail.com>
2016-04-19 18:25:20 -0400

    This commit fixes the issue that wouldn't read the entered password. I changed the user params to require "password" instead of "encrypted password". Now it works.
    And i added a new field to the form.

--- Group2/app/controllers/users_controller.rb
+++ Group2/app/controllers/users_controller.rb
@@ -69,6 +69,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:email, :encrypted_password, :user_type_id)
+      params.require(:user).permit(:email, :password, :user_type_id)
     end
 end
