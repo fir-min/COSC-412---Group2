5927a08a1334951c8ebbd7a647bb287e634e345b
b3bry <bateoa1@students.towson.edu>
2016-04-23 03:38:30 -0400

    Switched databases from sqlite3 to mysql.

--- Group2/config/routes.rb
+++ Group2/config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-
+  devise_for :users
   resources :users
   resources :charges
   resources :purchases
   resources :inventories
-  devise_for :users
   match ':controller(/:action(/:id(.:format)))', :via => :get
 
 
