56818c73fffb4d807e780dfa4d4385c279007b71
Ted Smith III <tsmith1119s@gmail.com>
2016-05-06 15:22:28 -0400

    Push that's forced on me
    I think it's updates that been made

--- Group2/db/schema.rb
+++ Group2/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160503071555) do
+ActiveRecord::Schema.define(version: 20160506024808) do
 
   create_table "budget_manages", force: :cascade do |t|
     t.string   "department"
@@ -87,6 +87,15 @@ ActiveRecord::Schema.define(version: 20160503071555) do
 
   add_index "mailboxer_receipts", ["notification_id"], name: "index_mailboxer_receipts_on_notification_id"
   add_index "mailboxer_receipts", ["receiver_id", "receiver_type"], name: "index_mailboxer_receipts_on_receiver_id_and_receiver_type"
+
+  create_table "products", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.float    "price"
+    t.string   "image"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "purchases", force: :cascade do |t|
     t.string   "name"
