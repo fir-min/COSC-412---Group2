4ac353d03fcd2b824c36123b67bd16bedc437259
Ted Smith III <tsmith1119s@gmail.com>
2016-05-10 18:34:07 -0400

    These are some updates for audit, that will create forms and gather the information from those forms

--- Group2/app/controllers/audit_controller.rb
+++ Group2/app/controllers/audit_controller.rb
@@ -1,16 +1,50 @@
 class AuditController < ApplicationController
   before_filter :authenticate_user!
-  def request_doc 
-    
+  def request_doc
+    @purch = Purchase.all
+
+    if params[:deptNo] == 0
+
+      @temp = "Values were empty";
+      @temp1 = "Values were empty";
+      @temp2 = "Values were empty";
+
+
+
+    else
+      if params[:deptNo] !=nil
+        @temp = params[:purchases][:start];
+        @temp1 = params[:purchases][:end];
+        @temp2 = params[:deptNo];
+
+        # use this for searching for specific models that fit within this range.
+        # This can be copied and paster
+      else
+
+        @temp = "Values were empty";
+        @temp1 = "Values were empty";
+        @temp2 = params[:deptNo];
+      end
+
+
+      #within this case save the variable @temp within a model with the timestamp
+    end
+
   end
-  
-    
-  def correct 
-  end 
-  
-  
-  def note 
-    
+
+
+  def correct
+  end
+
+
+  def note
+      if params[:noteText] == nil
+        @temp = "Values were empty";
+      else
+        @temp = params[:noteText];
+
+        #within this case save the variable @temp within a model with the timestamp
+      end
   end
 
   def audit
@@ -25,3 +59,4 @@ class AuditController < ApplicationController
 
   end
 end
+
